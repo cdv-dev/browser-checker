3acf3e1f1378c5760decbf66199d6a41b78616c2
Dmitriy Chernysh <dmitriy.chernysh@gmail.com>
2014-09-22 21:08:59 +0300

    Update config.js

--- browser-checker/source/config.js
+++ browser-checker/source/config.js
@@ -2,35 +2,35 @@ var allowedBrowsers = [
   {
       name : "Chrome",
       minVersion : 38,
-      downloadLink : "//www.google.com/chrome/browser/",
+      downloadLink : "https://www.google.com/chrome/browser/",
       iconLink : "../source/img/chrome.png",
       regexp : /(chrome)[ \/]([\w.]+)/i
    },
    {
        name : "Firefox",
        minVersion : 0,
-       downloadLink : "//www.mozilla.org/ru/firefox/",
+       downloadLink : "https://www.mozilla.org/ru/firefox/",
        iconLink : "../source/img/firefox.png",
        regexp : /(firefox)[\/]([\w.]+)/i
    },
     {
         name : "Safari",
         minVersion : 0,
-        downloadLink : "//www.apple.com/safari/",
+        downloadLink : "https://www.apple.com/safari/",
         iconLink : "../source/img/safari.png",
         regexp : /(safari)[\/]([\w.]+)/i
     },
     {
         name : "Opera",
         minVersion : 0,
-        downloadLink : "//www.opera.com/",
+        downloadLink : "https://www.opera.com/",
         iconLink : "../source/img/opera.png",
         regexp : /(opera)(?:.*version|)[ \/]([\w.]+)/i
     },
     {
         name : "IE",
         minVersion : 9,
-        downloadLink : "//windows.microsoft.com/ru-ru/internet-explorer/download-ie",
+        downloadLink : "https://windows.microsoft.com/ru-ru/internet-explorer/download-ie",
         iconLink : "../source/img/ie.png",
         regexp : /(msie)|(?:rv:)([\s][\w.]+|[\w.]+)/i  ///(msie|rv:)([\s][\w.]+|[\w.]+)/i  ///(msie) ([\w.]+)/
     }
