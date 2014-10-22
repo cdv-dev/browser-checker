fab4075d5eef3503d640ea46e3b37f11b7a26005
ChernyshDV <dmitriy.chernysh@creditdnepr.com>
2014-10-22 17:46:44 +0300

    Fixes for IE

--- source/checker.js
+++ source/checker.js
@@ -8,16 +8,14 @@
         for (i = 0, j = cfg.allowedBrowsers.length; i < j; i++) {
             var match = cfg.allowedBrowsers[i].regexp.exec(ua);
             if (match !== null) {
-                if (parseFloat(match[2]) >= parseFloat(cfg.allowedBrowsers[i].minVersion)) {
-                    b = true;
-                }
+                b = (parseFloat(match[2]) >= parseFloat(cfg.allowedBrowsers[i].minVersion));
                 break;
             }
         }
         return b;
     };
 
-    var setHtml = function() {
+   var setHtml = function() {
         if (cfg.allowedBrowsers.length === 0) return;
         var oBody = window.document.body;
         var msgDiv = document.createElement("div");
@@ -25,48 +23,54 @@
         var subDiv = document.createElement("div");
         var uaDiv = document.createElement("div");
 
-        var p, a, atrrHref, icon, img;
+        var p, a, atrrHref, icon, img, span;
 
         uaDiv.style.color = "#b8b8b8";
-        uaDiv.innerText = ua;
+        uaDiv.innerHTML = ua;
 
         msgDiv.style.width = "40%";
-        msgDiv.style.marginLeft = "auto";
-        msgDiv.style.marginRight = "auto";
+        msgDiv.style.position = "relative";
+        msgDiv.style.left = "50%";
+        msgDiv.style.marginLeft = "-20%";
         msgDiv.style.textAlign = "center";
         msgDiv.style.marginTop = "50px";
         msgDiv.style.paddingBottom = "25px";
         for (var res in cfg.locres) {
             p = document.createElement("p");
             p.style.fontSize = "20px";
-            p.innerText = cfg.locres[res];
+            p.innerHTML = cfg.locres[res];
             msgDiv.appendChild(p);
         }
         div.appendChild(msgDiv);
 
         for (var param in cfg.allowedBrowsers) {
             p = document.createElement("p");
+            p.style.padding = "10px";
             a = document.createElement("a");
+            a.style.fontSize = "24px"; //для FF
+            span = document.createElement("span");
             atrrHref = cfg.allowedBrowsers[param].downloadLink;
             a.setAttribute("href", (atrrHref !== "") ? atrrHref : "#");
-            a.style.fontWeight = "700";
-            a.style.fontSize = "24px";
+            span.style.fontWeight = "700";
+            span.style.fontSize = "24px";
+            span.style.paddingLeft = "0.5em";
             icon = cfg.allowedBrowsers[param].iconLink;
             if (icon !== "") {
                 img = document.createElement("img");
                 img.setAttribute("src", icon);
                 img.style.verticalAlign = "middle";
-                img.style.paddingRight = "1em";
                 img.style.border = "none";
                 a.appendChild(img);
             }
-            a.innerHTML = a.innerHTML + cfg.allowedBrowsers[param].name + " " + cfg.allowedBrowsers[param].minVersion + "+";
+            span.innerHTML = span.innerHTML + cfg.allowedBrowsers[param].name + " " + cfg.allowedBrowsers[param].minVersion + "+";
+            a.appendChild(span);
             p.appendChild(a);
             subDiv.appendChild(p);
         }
 
-        subDiv.style.marginLeft = "auto";
-        subDiv.style.marginRight = "auto";
+        subDiv.style.position = "relative";
+        subDiv.style.left = "50%";
+        subDiv.style.marginLeft = "-150px";
         subDiv.style.width = "300px";
         div.appendChild(subDiv);
         div.style.width = "100%";
